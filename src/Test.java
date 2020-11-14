2bc4fd1e5b703c845ba9adf47caa64084d11c5d1
VISWANATH-PUVVULA <viswanathpuvvula@gmail.com>
2020-11-14 21:32:29 +0530

    First commit

---
+++ src/Test.java
@@ -0,0 +1,22 @@
+
+public class Test {
+
+		String user;
+		String password;
+	}
+
+ class DBHandler {
+	
+	 Test configureDB(String user, String password) {
+				// insert code here
+		 return Test;
+			}
+
+ 
+
+	public static void main(String[] args) {
+					DBHandler r = new DBHandler();
+					Test dbConf = r.configureDB("manager", "manager");
+				}
+
+}
